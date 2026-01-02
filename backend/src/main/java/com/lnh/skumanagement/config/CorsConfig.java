445b7f2eb2c1fe8512bed0a716f1cfcf17e925bb
linenandthehavens-cell <linenandthehavens@gmail.com>
2026-01-02 22:37:50 +0530

    adding logs

--- backend/src/main/java/com/lnh/skumanagement/config/CorsConfig.java
+++ backend/src/main/java/com/lnh/skumanagement/config/CorsConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
-    }
+    }*/
 }
